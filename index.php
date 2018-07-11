60f0dc05e972856c2bbe2e54d91796e81c65df60
ShevchukLeo <leoshef@gmail.com>
2018-07-11 22:40:04 +0300

    11/07 22/39

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ $array_menu = array(
 );
 ?>
         <nav>
-            <ul>
+            <ul class = "top-menu">
                 <li><a href='<?=$array_menu [0] ["href"]?>'><?=$array_menu [0] ["link"]?></a></li>
                 <li><a href='<?=$array_menu [1] ["href"]?>'><?=$array_menu [1] ["link"]?></a></li>
                 <li><a href='<?=$array_menu [2] ["href"]?>'><?=$array_menu [2] ["link"]?></a></li>
@@ -36,69 +36,63 @@ $array_menu = array(
             </ul>
         </nav>
     </header>
-    <pol>
-        <div id="pol">
-        </div>
-    </pol>
-    <block1>
+    <div class="container">
         <div id="block1">
-            <a href="#"><img src="images/img1.png" width="920" height="386"></a>
+            <a href="#"><img src="images/img1.png" width="939" height="397"></a>
         </div>
-    </block1>
-    <div id="blocks">
-        <div id="block2">
-            <a href="#"><img src="images/b21.png" width="141" height="140"></a>
-            <a href="#"><img src="images/b22.png" width="141" height="140"></a>
-            <a href="#"><img src="images/b23.png" width="141" height="140"></a>
-            <a href="#"><img src="images/b24.png" width="141" height="140"></a>
-            <a href="#"><img src="images/b25.png" width="141" height="140"></a>
-            <b href="#"><img src="images/b26.png" width="141" height="140"></b>
-        </div>
-        <block3>
-            <div id="block3">
-                <div id="column1">
-                    <a href="#"><img src="images/b31.png" width="50" height="50"></a>
-                    <h1>CHECK OUT MY LATEST PORTFOLIO ITEMS</h1>
-                    <p>Maecenas ipsum metus, semper  henrerit  varius mattis, congue  sit amet tellus.
-                        Aliquam ullamcorper dui sed magna posue re ut elementum enim  rutrum.
-                        Nam mi erat, porta id ultrices nec, pellentesque vel nunc.
-                        Cras varius fermentum iaculis.
-                        Aenean sodales nibh non lectus tempor a interdum justo ultricies</p>
-                    <button type="submit">MORE</button>
-                </div>
-                <div id="column2">
-                <a href="#"><img src="images/b32.png" width="50" height="50"></a>
-                <h1>OFFER TOP NOTCH QUALITY WORK & SERVICES</h1>
-                    <p>Maecenas ipsum metus, semper  henrerit  varius mattis, congue  sit amet tellus.
-                        Aliquam ullamcorper dui sed magna posue re ut elementum enim  rutrum.
-                        Nam mi erat, porta id ultrices nec, pellentesque vel nunc.
-                        Cras varius fermentum iaculis.
-                        Aenean sodales nibh non lectus tempor a interdum justo ultricies</p>
-                    <button type="submit">MORE</button>
-                </div>
-                <div id="column3">
-                <a href="#"><img src="images/b33.png" width="50" height="50"></a>
-                <h1>FOLLOW ME  ON TWITTER FACEBOOK & PRINTEREST</h1>
-                    <p>Maecenas ipsum metus, semper  henrerit  varius mattis, congue  sit amet tellus.
-                        Aliquam ullamcorper dui sed magna posue re ut elementum enim  rutrum.
-                        Nam mi erat, porta id ultrices nec, pellentesque vel nunc.
-                        Cras varius fermentum iaculis.
-                        Aenean sodales nibh non lectus tempor a interdum justo ultricies</p>
-                    <button type="submit">MORE</button>
-                </div>
+        <div id="blocks">
+            <div id="block2">
+                <a href="#"><img src="images/b21.png" width="141" height="140"></a>
+                <a href="#"><img src="images/b22.png" width="141" height="140"></a>
+                <a href="#"><img src="images/b23.png" width="141" height="140"></a>
+                <a href="#"><img src="images/b24.png" width="141" height="140"></a>
+                <a href="#"><img src="images/b25.png" width="141" height="140"></a>
+                <b href="#"><img src="images/b26.png" width="141" height="140"></b>
             </div>
-        </block3>
-        <div id="block4">
-            <h1>RECENT WORKS</h1>
+            <block3>
+                <div id="block3">
+                    <div id="column1">
+                        <a href="#"><img src="images/b31.png" width="50" height="50"></a>
+                        <h1>CHECK OUT MY LATEST PORTFOLIO ITEMS</h1>
+                        <p>Maecenas ipsum metus, semper  henrerit  varius mattis, congue  sit amet tellus.
+                            Aliquam ullamcorper dui sed magna posue re ut elementum enim  rutrum.
+                            Nam mi erat, porta id ultrices nec, pellentesque vel nunc.
+                            Cras varius fermentum iaculis.
+                            Aenean sodales nibh non lectus tempor a interdum justo ultricies</p>
+                        <button type="submit">MORE</button>
+                    </div>
+                    <div id="column2">
+                    <a href="#"><img src="images/b32.png" width="50" height="50"></a>
+                    <h1>OFFER TOP NOTCH QUALITY WORK & SERVICES</h1>
+                        <p>Maecenas ipsum metus, semper  henrerit  varius mattis, congue  sit amet tellus.
+                            Aliquam ullamcorper dui sed magna posue re ut elementum enim  rutrum.
+                            Nam mi erat, porta id ultrices nec, pellentesque vel nunc.
+                            Cras varius fermentum iaculis.
+                            Aenean sodales nibh non lectus tempor a interdum justo ultricies</p>
+                        <button type="submit">MORE</button>
+                    </div>
+                    <div id="column3">
+                    <a href="#"><img src="images/b33.png" width="50" height="50"></a>
+                    <h1>FOLLOW ME  ON TWITTER FACEBOOK & PRINTEREST</h1>
+                        <p>Maecenas ipsum metus, semper  henrerit  varius mattis, congue  sit amet tellus.
+                            Aliquam ullamcorper dui sed magna posue re ut elementum enim  rutrum.
+                            Nam mi erat, porta id ultrices nec, pellentesque vel nunc.
+                            Cras varius fermentum iaculis.
+                            Aenean sodales nibh non lectus tempor a interdum justo ultricies</p>
+                        <button type="submit">MORE</button>
+                    </div>
+                </div>
+            </block3>
+            <div id="block4">
+                <h1>RECENT WORKS</h1>
+            </div>
+            <div id="block5">
+                <a href="#"><img src="images/b41.png" width="215" height="215"></a>
+                <a href="#"><img src="images/b42.png" width="215" height="215"></a>
+                <a href="#"><img src="images/b43.png" width="215" height="215"></a>
+                <b href="#"><img src="images/b44.png" width="215" height="215"></b>
+            </div>
         </div>
-        <div id="block5">
-            <a href="#"><img src="images/b41.png" width="215" height="215"></a>
-            <a href="#"><img src="images/b42.png" width="215" height="215"></a>
-            <a href="#"><img src="images/b43.png" width="215" height="215"></a>
-            <b href="#"><img src="images/b44.png" width="215" height="215"></b>
-        </div>
-    </div>
-    <footer>
         <div id="footer">
             <div id="about-us">
                 <hf>ABOUT JETRO</hf>
@@ -110,20 +104,16 @@ $array_menu = array(
             <div id="twitterf">
                 <hf>TWITTER WIDGET</hf>
                 <div>
-                    <p>
-                        <span >@pixelmater</span>
-                        <span> What an awesome design with great functionality :)</span>
-                    </p>
-                </div>
+                    <p><a name="@pixelmater"><a href="#">@designdude</a>What an awesome design with great functionality :)</p>
                     <time datetime="2012-10-23"><a href="#">about 1 hour ago</a></time>
-                <div>
-                    <author name="@designdude"><a href="#">@designdude</a>
-                    <p>What an awesome design with great functionality :)</p></author>
                 </div>
-                <time datetime="2012-10-23"><a href="#">about 2 hour ago</a></time>
+
                 <div>
-                    <p>Follow</p>
-                    <author name="@pixelmater"><a href="#">@pixelmater</a></author>
+                     <p><a name="@designdude"><a href="#">@designdude</a>What an awesome design with great functionality :)</p></a>
+                     <time datetime="2012-10-23"><a href="#">about 2 hour ago</a></time>
+                </div>
+                <div>
+                    <p>Follow<author name="@pixelmater"><a href="#">@pixelmater</a></author></p>
                 </div>
             </div>
             <div id="social">
@@ -133,11 +123,11 @@ $array_menu = array(
                 <div>
                     <p>information@jetro.com</br>1.222.333.4444</p>
                 </div>
-                
-               
+
+
                 <a href="http://plus.google.com/" class="social-icon google-plus"></a>
             </div>
         </div>
-    </footer>
+    </div>
 </body>
 </html>
